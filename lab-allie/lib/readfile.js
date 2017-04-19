963d819667512d543ae5f110865dc6f93b098b5b
Allie Grampa <agrampa@yahoo.com>
2017-04-19 16:51:06 -0700

    first 8 hex are display in console.log

--- lab-allie/lib/readfile.js
+++ lab-allie/lib/readfile.js
@@ -1,1 +1,19 @@
 'use strict';
+
+const Buffer = require('buffer').Buffer;
+const fs = require('fs');
+
+fs.readFile(`${__dirname}/../textfiles/one.txt`, function(err, data) {
+  if(err) throw err;
+  console.log(data.toString('hex', 0, 8));
+});
+
+fs.readFile(`${__dirname}/../textfiles/two.txt`, function(err, data) {
+  if(err) throw err;
+  console.log(data.toString('hex', 0, 8));
+});
+
+fs.readFile(`${__dirname}/../textfiles/three.txt`, function(err, data) {
+  if(err) throw err;
+  console.log(data.toString('hex', 0, 8));
+});
