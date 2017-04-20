205679ba78212e3b4853e6023dfe89b0c3a6db98
Allie Grampa <agrampa@yahoo.com>
2017-04-19 17:28:01 -0700

    three tests passing to make sure the return is equal to the first 8 characters

--- lab-allie/index.js
+++ lab-allie/index.js
@@ -1,2 +1,3 @@
 'use strict';
 
+const readfile = require('./lib/readfile.js');
--- lab-allie/test/readfile-test.js
+++ lab-allie/test/readfile-test.js
@@ -3,3 +3,35 @@
 const expect = require('chai').expect;
 const Buffer = require('buffer').Buffer;
 const fs = require('fs');
+
+describe('readfile.js', function() {
+  describe('#readFile', function() {
+    it('should return the first text file', function(done) {
+      fs.readFile(`${__dirname}/../textfiles/one.txt`, function(err, data) {
+        if(err) throw err;
+        let testData = data.toString('hex', 0, 8);
+        expect(testData).to.be.equal('54686973206f6e65');
+        done();
+      });
+    });
+    
+    it('should return the second text file next', function(done) {
+      fs.readFile(`${__dirname}/../textfiles/two.txt`, function(err, data) {
+        if(err) throw err;
+        let testData = data.toString('hex', 0, 8);
+        expect(testData).to.be.equal('426c696767697479');
+        done();
+      });
+    });
+    
+    it('should return the third text file last', function(done) {
+      fs.readFile(`${__dirname}/../textfiles/three.txt`,
+      function(err, data) {
+        if(err) throw err;
+        let testData = data.toString('hex', 0, 8);
+        expect(testData).to.be.equal('416e642068657265');
+        done();
+      });
+    });
+  });
+});
