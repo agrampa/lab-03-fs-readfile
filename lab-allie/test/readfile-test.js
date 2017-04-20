eadea0275d13bcf532e7b4c22923624ef1e9a854
Allie Grampa <agrampa@yahoo.com>
2017-04-20 09:48:00 -0700

    pushing into array works but test fails

--- lab-allie/test/readfile-test.js
+++ lab-allie/test/readfile-test.js
@@ -1,7 +1,6 @@
 'use strict';
 
 const expect = require('chai').expect;
-const Buffer = require('buffer').Buffer;
 const fs = require('fs');
 
 describe('readfile.js', function() {
@@ -33,5 +32,41 @@ describe('readfile.js', function() {
         done();
       });
     });
+    
+    let arrayOfThree = [];
+    
+    function getText1() {
+      fs.readFile(`${__dirname}/../textfiles/one.txt`, function(err, data) {
+        if(err) throw err;
+        arrayOfThree.push(data.toString('hex', 0, 8));
+        return arrayOfThree;
+      });
+    }
+    
+    function getText2() {
+      fs.readFile(`${__dirname}/../textfiles/two.txt`, function(err, data) {
+        if(err) throw err;
+        arrayOfThree.push(data.toString('hex', 0, 8));
+        return arrayOfThree;
+      });
+    }
+    
+    function getText3() {
+      fs.readFile(`${__dirname}/../textfiles/three.txt`, function(err, data) {
+        if(err) throw err;
+        arrayOfThree.push(data.toString('hex', 0, 8));
+        return arrayOfThree;
+      });
+    }
+    
+    getText3(getText2(getText1()));
+    
+    it('should push the expected results into an array', function(done) {
+      
+      expect(arrayOfThree).to.be.equal(['54686973206f6e65','426c696767697479','416e642068657265']);
+      
+      done();
+    });
   });
 });
+
